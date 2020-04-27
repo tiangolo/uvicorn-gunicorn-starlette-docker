0fa2279e801e255b43c5b46dffad1d0ac8798ef4
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-27 19:12:57 +0200

    :white_check_mark: Fix tests

--- tests/utils.py
+++ tests/utils.py
@@ -46,4 +46,4 @@ def get_logs(container: DockerClient) -> str:
 
 def get_response_text1() -> str:
     python_version = os.getenv("PYTHON_VERSION")
-    return f"Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {python_version}"
+    return f"Hello world! From Starlette running on Uvicorn with Gunicorn. Using Python {python_version}"
