ed1c8b74e1c8285b5ce5a4321550d483aeb1f72b
Sebastián Ramírez <tiangolo@gmail.com>
2026-09-09 18:37:30 +0200

    🐛 Fix bundled app compatibility with Starlette 1.x

--- docker-images/app/main.py
+++ docker-images/app/main.py
@@ -2,13 +2,14 @@ import sys
 
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.routing import Route
 
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
-app = Starlette()
 
-
-@app.route("/")
 async def homepage(request):
     message = f"Hello world! From Starlette running on Uvicorn with Gunicorn. Using Python {version}"
     return JSONResponse({"message": message})
+
+
+app = Starlette(routes=[Route("/", homepage)])
